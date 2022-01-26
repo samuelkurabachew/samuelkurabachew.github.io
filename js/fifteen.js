8f03ac347b7e43af63f5f3ab2bfcf0a7eebb78d4
Samuel <samkura47@gmail.com>
2022-01-25 19:19:28 -0600

    adequate commenting

--- js/fifteen.js
+++ js/fifteen.js
@@ -1,11 +1,12 @@
 "use strict";
 $(document).ready(function () {
-    var count = 0;
-    var arr = []; 
-    var EMPTY_SQUARE = {};
-    EMPTY_SQUARE.leftx = "300px";
-    EMPTY_SQUARE.toppx = "300px";
+    var count = 0; //count variable for left and right positioning
+    var arr = []; //array used for storing the filled position for shuffling
+    var EMPTY_SQUARE = {}; //store and updates empty div
+    EMPTY_SQUARE.leftx = "300px"; //initial left position
+    EMPTY_SQUARE.toppx = "300px"; //initial top position
 
+    //set the position and css for the filled squares
     $('#puzzlearea div').each(function () {
         var filledSquares = {};
         var x = ((count % 4) * 100);
@@ -16,10 +17,11 @@ $(document).ready(function () {
         filledSquares.leftpx = leftpx;
         filledSquares.toppx = toppx;
         arr.push(filledSquares);
+        // set image background
         $(this).css({ "left": leftpx, "top": toppx, "backgroundImage": 'url("./images/background.jpeg")', "backgroundPosition": -x + 'px ' + (-y) + 'px' });
         count++;
     });
-
+    //check if the square can be moved, and swap the value for emptysquare
     $('.puzzlepiece').click(function () {
         var changedleft = $(this).css("left");
         var changedtop = $(this).css("top");
@@ -35,7 +37,7 @@ $(document).ready(function () {
 
         }
     });
-
+    //change the index of the divs stored as objects in the array (shuffle)
     $('#shufflebutton').click(function () {
         var i = 0, j = 0, suff = 0, temp = null;
 
@@ -46,6 +48,7 @@ $(document).ready(function () {
             arr[j] = temp;
         }
 
+        //set css after suffling
         $('#puzzlearea div').each(function () {
             $(this).css({ "left": arr[suff].leftpx, "top": arr[suff].toppx });
             suff++;
@@ -57,6 +60,7 @@ $(document).ready(function () {
         EMPTY_SQUARE.toppx = "300px";
     });
 
+    //Change css and remove css on hover
     $('.puzzlepiece').hover(function () {
         var changedleft = $(this).css("left");
         var changedtop = $(this).css("top");
@@ -70,8 +74,10 @@ $(document).ready(function () {
         $(this).removeClass('movablepiece');
     });
 
+     //find the neighbouring elements on the left
     var clickableleft = function (changedleft, changedtop) {
         if (parseInt(changedtop) === parseInt(EMPTY_SQUARE.toppx)) {
+            //check whether element neighbour is an empty div
             if (parseInt(changedleft) + 100 === parseInt(EMPTY_SQUARE.leftx) || parseInt(changedleft) - 100 === parseInt(EMPTY_SQUARE.leftx)) {
                 return true;
             } else {
@@ -80,8 +86,10 @@ $(document).ready(function () {
         }
     };
 
+    //helper function for finding the neighbouring elements on the right
     var clickableTop = function (changedleft, changedtop) {
         if (parseInt(changedleft) === parseInt(EMPTY_SQUARE.leftx)) {
+            //check whether element neighbour is an empty div
             if (parseInt(changedtop) + 100 === parseInt(EMPTY_SQUARE.toppx) || parseInt(changedtop) - 100 === parseInt(EMPTY_SQUARE.toppx)) {
                 return true;
             } else {
