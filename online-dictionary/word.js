31deec266de901b1e1f7a597836f9c49e0f98670
Samuel <samkura47@gmail.com>
2022-02-01 20:30:07 -0600

    taking care of variable naming

--- online-dictionary/word.js
+++ online-dictionary/word.js
@@ -1,17 +1,17 @@
-var db = require('mysql');
-var con = db.createConnection({
+const db = require('mysql');
+const connection = db.createConnection({
     host: "localhost",
     user: "root",
     database: "entries"
 });
-con.connect(function (err) {
+connection.connect(function (err) {
     if (err) throw err;
 
 })
 exports.lookup = function search(query, find) {
 
     var statement = "SELECT * FROM entries WHERE word LIKE '" + query + "'";
-    con.query(statement, function (err, result) {
+    connection.query(statement, function (err, result) {
         if (err) throw err;
         return find(result);
     })
